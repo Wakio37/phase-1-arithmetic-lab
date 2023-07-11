6dad8b076e21aeb00aeb4bbabdc821ecefa63a7b
Peris Wakio <peris.kingori@student.moringaschool.com>
2023-07-11 14:36:44 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31
+let num2 = 2
+let multiply= num1*num2
+console.log(multiply)
 
-// Write your code here
+
+
+let random = Math.ceil(Math.random())
+console.log(random)
+
+
+let num3 = 16
+let num4 = 6
+let mod = 16 % 6
+console.log(mod)
+
+let max = Math.max(13,12,20)
+console.log(max)
